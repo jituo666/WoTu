8b2710fc0e48047a21040b3b465b20427a63a383
xuanjituo <xuanjituo@camera360.com>
2014-01-23 23:27:43 +0800

    add slotview layout class

---
+++ src/com/wotu/view/layout/BaseLayout.java
@@ -0,0 +1,19 @@
+
+package com.wotu.view.layout;
+
+public class BaseLayout {
+    private int mVisibleStart;
+    private int mVisibleEnd;
+
+    private int mSlotCount;
+    private int mSlotWidth;
+    private int mSlotHeight;
+    private int mSlotGap;
+
+    private int mWidth;
+    private int mHeight;
+
+    private int mUnitCount;
+    private int mContentLength;
+    private int mScrollPosition;
+}
